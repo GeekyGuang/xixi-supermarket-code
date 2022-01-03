6399f6f9bac93b5916ce37fb0619e2cd456999b8
TravisWongX <travis.x.wong@gmail.com>
2022-01-03 15:16:16 +0800

    使用:deep设置组件内部样式

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,10 +1,37 @@
 <template>
-  shop
+  <div class="shop-wrapper">
+    <shopInfo :info="item"/>
+  </div>
+
 </template>
 
 <script lang="ts">
-export default {}
+import ShopInfo from '@/components/ShopInfo.vue'
+export default {
+  components: { ShopInfo },
+  setup(){
+    const item = {
+      _id: "1",
+      name: "沃尔玛",
+      imgUrl: require('@/assets/images/wowmall.png'),
+      sales: "10000",
+      expressLimit: "0",
+      expressPrice: "5",
+      slogan: "VIP尊享满89减4元运费券"
+    }
+    return {
+      item
+    }
+  }
+  }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.shop-wrapper {
+  padding: 0 18px;
+
+  :deep(.shop-info) {
+    border-bottom: none;
+  }
+}
 </style>
