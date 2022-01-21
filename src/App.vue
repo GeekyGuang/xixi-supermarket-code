650f65c44cc0fd7d53eea160321ca3b9f7725298
travis <travis.x.wong@gmail.com>
2022-01-21 12:06:09 +0800

    添加媒体查询

--- src/App.vue
+++ src/App.vue
@@ -8,5 +8,14 @@ export default {
 </script>
 
 <style lang="scss">
-
+  @media(min-width: 500px) {
+    body {
+      background: black;
+    }
+    #app {
+      max-width: 375px;
+      margin: 0 auto;
+      background: white;
+    }
+  }
 </style>
